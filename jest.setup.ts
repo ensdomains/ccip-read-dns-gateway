a020775053c4acd058b0414e449c7db50c2e0b2c
Muhammed Tanrıkulu <md.tanrikulu@gmail.com>
2026-01-20 13:51:53 +0100

    update e2e tests for ExtendedDNSResolver and simplify CI

--- jest.setup.ts
+++ jest.setup.ts
@@ -3,3 +3,5 @@ import { TextEncoder, TextDecoder } from 'util';
 
 (global as any).TextEncoder = TextEncoder;
 (global as any).TextDecoder = TextDecoder;
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+(global as any).fetch = require('node-fetch');
